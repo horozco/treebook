0cafdb57bfcd5def9055a15634e393038edf83b7
Hernan Orozco <horozco15@gmail.com>
2012-11-14 13:57:14 -0500

    add to repository all files of treebook project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'c2dda416a36b4ffe48b04f92e067e83b62b3b97dd74fde8febc5d3f8e44be9ffae8c7be006808358b7e7ab3b3cf5c7bb7b06f3eb4d213d68c4c7852e7b5541b3'
