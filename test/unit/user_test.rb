9d537eefb05ff7c6208bc0e36e3cb6a5820e9eea
Hernan Orozco <horozco15@gmail.com>
2012-11-16 16:17:56 -0500

    Added Some test
    view modifications
    add image to sign in and sign up form

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
+#class UserTest < ActiveSupport::TestCase
+#  test "a user should enter a first name" do
+#     user = User.new
+#     user.email = "prueba@domain.com"
+#     user.password = "123456"
+#     assert user.save
+#  end
+
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+     user = User.new
+     assert !user.save
+     assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+     user = User.new
+     assert !user.save
+     assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+     user = User.new
+     assert !user.save
+     assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+     user = User.new
+     user.first_name = "Hernan"
+     user.last_name = "Hernan"
+     user.email = "Hernan"
+     user.password = "Hernan"
+     user.password_confirmation = "Hernan"
+     user.profile_name = "name"
+     #puts user.errors.inspect
+     assert !user.save
+     #assert !user.errors[:profile_name].empty?
+  end
+
 end
